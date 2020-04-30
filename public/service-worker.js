9b4a500499f558442ac3686c15b20d58efe12986
Parr <tedblount01@gmail.com>
2020-04-29 19:12:23 -0500

    ommitted problematic links / and flavacon

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,9 @@
 const FILES_TO_CACHE = [
-  "/",
   "/index.html",
   "/index.js",
   "/db.js",
   "/styles.css",
   "/manifest.webmanifest",
-  "/favicon.ico",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
